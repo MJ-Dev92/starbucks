c0e4a5bcaaae258f18410d1814c1772603ed3b75
MJ-Dev92 <minjae920802@naver.com>
2022-10-20 13:27:32 +0900

    Start project

---
+++ js/youtube.js
@@ -0,0 +1,22 @@
+
+let tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function onYouTubeIframeAPIReady() {
+  new YT.Player('player', {
+    videoId: 'u3ybWiEUaUU',
+    playerVars : {
+      autoplay : true,
+      loop : true,
+      playlist : 'u3ybWiEUaUU'
+    },
+    events : {
+      onReady: function (event) {
+        event.target.mute()
+      }
+    }
+  });
+}
